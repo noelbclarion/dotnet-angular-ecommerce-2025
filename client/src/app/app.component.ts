8fb1ce7dfaa0b2613058711ea506d356ea157701
noel <noelbclarion@gmail.com>
2025-02-14 09:54:28 +0800

    Setting up the folder structure and creating components

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';;
+import { Component } from '@angular/core';
+import { HeaderComponent } from "./layout/header/header.component";;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [HeaderComponent]
 })
 export class AppComponent {
   title = 'Skinet';
